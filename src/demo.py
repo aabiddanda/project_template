55843ec1bbde8e0d566a9dc64366e7d5548a1625
Arjun Biddanda <aabiddanda@gmail.com>
2020-10-26 23:18:53 -0500

    added in new template directory with precommits and automated testing

---
+++ src/demo.py
@@ -0,0 +1,6 @@
+"""Test src for functions."""
+
+
+def simple(x=1):
+    """Test function for addition by one."""
+    return x + 1
---
+++ tests/test_demo.py
@@ -0,0 +1,15 @@
+"""Test setup."""
+
+from hypothesis import given
+import hypothesis.strategies as st
+import sys
+import os
+
+sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/../src/"))
+import demo  # noqa: E402
+
+
+@given(st.integers())
+def test_add_subtract(x):
+    """Test for the +1 - 1 yielding the argument."""
+    assert (demo.simple(x) - 1) == x
